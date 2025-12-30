2c8ef80c625842585b425304a8cda7b684820a29
jFrostyBoy <jfrostyboy01@gmail.com>
2025-12-30 15:01:31 +0200

    Add sound feedback to reload command

--- src/main/java/jfbdev/jupgradableeconomy/commands/ReloadCommand.java
+++ src/main/java/jfbdev/jupgradableeconomy/commands/ReloadCommand.java
@@ -4,6 +4,7 @@ import jfbdev.jupgradableeconomy.JUpgradableEconomy;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class ReloadCommand implements CommandExecutor {
@@ -18,12 +19,14 @@ public class ReloadCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (!sender.hasPermission("jupgradableeconomy.admin")) {
             sender.sendMessage(plugin.getMessage("reload-permission"));
+            plugin.playSound((Player) sender, "message");
             return true;
         }
 
         plugin.reloadConfig();
         plugin.saveAllData();
         sender.sendMessage(plugin.getMessage("reload-success"));
+        plugin.playSound((Player) sender, "message");
         return true;
     }
 }
